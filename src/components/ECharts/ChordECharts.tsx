3f474c668a765986139db3731d39755716a46dcd
Ed Pfromer <epfromer@gmail.com>
2020-07-12 16:29:10 -0600

    update

---
+++ src/components/ECharts/ChordECharts.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { ECharts } from 'react-native-echarts-wrapper'
+
+export default function ChordECharts() {
+  const config = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1220],
+        type: 'line',
+      },
+    ],
+  }
+
+  return (
+    <View style={styles.container}>
+      <ECharts option={config} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
--- src/views/ChordView.tsx
+++ src/views/ChordView.tsx
@@ -1,27 +1,12 @@
-import { Body, Card, CardItem } from 'native-base'
 import React from 'react'
-import { ScrollView, Text } from 'react-native'
 import AppHeader from '../components/AppHeader'
+import ChordECharts from '../components/ECharts/ChordECharts'
 
 export default function ChordView() {
   return (
     <>
       <AppHeader title="Chord" />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Card>
-          <CardItem header>
-            <Text>ChordView</Text>
-          </CardItem>
-          <CardItem>
-            <Body>
-              <Text>some text</Text>
-            </Body>
-          </CardItem>
-          <CardItem footer>
-            <Text>GeekyAnts</Text>
-          </CardItem>
-        </Card>
-      </ScrollView>
+      <ChordECharts />
     </>
   )
 }
