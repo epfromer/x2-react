8d5c44c602c54783466bb21bb441538fe8ef161c
Ed Pfromer <epfromer@gmail.com>
2020-07-12 18:56:51 -0600

    update

--- src/views/PieView.tsx
+++ src/views/PieView.tsx
@@ -1,27 +1,12 @@
-import { Body, Card, CardItem } from 'native-base'
 import React from 'react'
-import { ScrollView, Text } from 'react-native'
 import AppHeader from '../components/AppHeader'
+import PieECharts from '../components/ECharts/PieECharts'
 
 export default function PieView() {
   return (
     <>
       <AppHeader title="Pie" />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Card>
-          <CardItem header>
-            <Text>PieView</Text>
-          </CardItem>
-          <CardItem>
-            <Body>
-              <Text>some text</Text>
-            </Body>
-          </CardItem>
-          <CardItem footer>
-            <Text>GeekyAnts</Text>
-          </CardItem>
-        </Card>
-      </ScrollView>
+      <PieECharts />
     </>
   )
 }
