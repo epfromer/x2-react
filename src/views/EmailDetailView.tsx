0881af58b5269da2fa17649ab73c4cd1f75c7dad
Ed Pfromer <epfromer@gmail.com>
2020-07-28 09:11:16 -0600

    update

--- src/views/EmailDetailView.tsx
+++ src/views/EmailDetailView.tsx
@@ -1,20 +1,15 @@
-import {
-  Spinner,
-  Left,
-  Right,
-  Button,
-  Icon,
-  Body,
-  Header,
-  Title,
-} from 'native-base'
-import { Col, Row, Grid } from 'react-native-easy-grid'
+import { Button, Icon, Spinner } from 'native-base'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import Highlighter from 'react-native-highlight-words'
 import { useSelector } from 'react-redux'
 import AppHeader from '../components/AppHeader'
-import { getEmailById } from '../store'
+import {
+  getEmailById,
+  getEmailIndex,
+  getPreviousEmail,
+  getNextEmail,
+} from '../store'
 import { REACT_APP_EMAIL_SERVER } from '../store/env'
 import { Email, RootState } from '../store/types'
 
@@ -22,7 +17,7 @@ interface Props {
   route: any
   navigation: any
 }
-export default function EmailDetailView({ route, navigation }: Props) {
+export default function EmailDetailView({ route }: Props) {
   const darkMode = useSelector((state: RootState) => state.darkMode)
   const [loading, setLoading] = useState(false)
   const themePrimaryColor = useSelector(
@@ -37,22 +32,39 @@ export default function EmailDetailView({ route, navigation }: Props) {
   const from = useSelector((state: RootState) => state.from)
   const subject = useSelector((state: RootState) => state.subject)
   const body = useSelector((state: RootState) => state.body)
-  const highlightedTerms: Array<string> = []
-  if (allText) highlightedTerms.push(allText)
-  if (to) highlightedTerms.push(to)
-  if (from) highlightedTerms.push(from)
-  if (subject) highlightedTerms.push(subject)
-  if (body) highlightedTerms.push(body)
+  const totalEmails = useSelector((state: RootState) => state.emails?.length)
+  const emailIndex = useSelector((state: RootState) =>
+    getEmailIndex(state, route.params.id)
+  )
+  const previousEmailId = useSelector((state: RootState) => {
+    const e = getPreviousEmail(state, route.params.id)
+    return e ? e._id : null
+  })
+  const nextEmailId = useSelector((state: RootState) => {
+    const e = getNextEmail(state, route.params.id)
+    return e ? e._id : null
+  })
+
   const styles = StyleSheet.create({
     container: {
       flex: 1,
     },
     emailHeader: {
-      height: 30,
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      flexDirection: 'row',
+    },
+    center: {
+      flex: 1,
+      alignItems: 'center',
+    },
+    spaceBetween: {
+      justifyContent: 'space-between',
+      flexDirection: 'row',
     },
     title: {
       fontSize: 20,
-      padding: 10,
+      paddingLeft: 10,
       color: darkMode ? 'white' : 'black',
     },
     fields: {
@@ -87,6 +99,13 @@ export default function EmailDetailView({ route, navigation }: Props) {
     },
   })
 
+  const highlightedTerms: Array<string> = []
+  if (allText) highlightedTerms.push(allText)
+  if (to) highlightedTerms.push(to)
+  if (from) highlightedTerms.push(from)
+  if (subject) highlightedTerms.push(subject)
+  if (body) highlightedTerms.push(body)
+
   function doFetch() {
     setLoading(true)
     const url = `${REACT_APP_EMAIL_SERVER}/email/${route.params.id}`
@@ -118,20 +137,13 @@ export default function EmailDetailView({ route, navigation }: Props) {
   function EmailHeader() {
     return (
       <View style={styles.emailHeader}>
-        <Grid>
-          <Row>
-            <Col>
-              <Button transparent onPress={() => console.log('foo')}>
-                <Icon type="MaterialIcons" name="brightness-7" />
-              </Button>
-            </Col>
-            <Col>
-              <Button transparent onPress={() => console.log('foo')}>
-                <Icon type="MaterialIcons" name="brightness-7" />
-              </Button>
-            </Col>
-          </Row>
-        </Grid>
+        <Button transparent onPress={() => console.log('foo')}>
+          <Icon type="MaterialIcons" name="arrow-back" />
+        </Button>
+        <Text>foo</Text>
+        <Button transparent onPress={() => console.log('foo')}>
+          <Icon type="MaterialIcons" name="arrow-forward" />
+        </Button>
       </View>
     )
   }
