79be5cc31ddac3ebb4868742a70b29f20b28527f
Ed Pfromer <epfromer@gmail.com>
2020-08-05 12:41:08 -0600

    starting move to monorepo

--- src/store/constants.tsx
+++
@@ -1,3 +0,0 @@
-export const MAX_FROM_LENGTH = 25
-export const MAX_SUBJECT_LENGTH = 40
-export const EMAIL_LIST_PAGE_LENGTH = 20
--- src/store/env.ts
+++
@@ -1,1 +0,0 @@
-export const REACT_APP_EMAIL_SERVER = 'https://x2-server-mongodb.herokuapp.com'
