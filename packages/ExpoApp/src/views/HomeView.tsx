793d27dfae7b04e782d2554834f508600a8d0665
Ed Pfromer <epfromer@gmail.com>
2020-08-07 12:36:53 -0600

    UPDATE

--- packages/ExpoApp/src/views/HomeView.tsx
+++ packages/ExpoApp/src/views/HomeView.tsx
@@ -6,6 +6,7 @@ import {
   PIE,
   POLAR,
   SEARCH,
+  SETTINGS,
   TREEMAP,
   VOLUMETIMELINE,
   WORDCLOUD,
@@ -23,77 +24,77 @@ export default function HomeView({ navigation }: Props) {
       <ScrollView>
         <HomeCard
           navigation={navigation}
-          image={PIE}
+          image={PIE as any}
           title="Pie"
           description="Pie chart of email volume of Enron key contacts."
           link="PieView"
         />
         <HomeCard
           navigation={navigation}
-          image={BARCHART}
+          image={BARCHART as any}
           title="Bar"
           description="Bar chart of email volume of Enron key contacts."
           link="BarView"
         />
         <HomeCard
           navigation={navigation}
-          image={CHORD}
+          image={CHORD as any}
           title="Chord"
           description="Chord diagram of Enron key contact communication."
           link="ChordView"
         />
         <HomeCard
           navigation={navigation}
-          image={WORDCLOUD}
+          image={WORDCLOUD as any}
           title="Word Cloud"
           description="Word cloud of fraudulent project names."
           link="WordCloudView"
         />
         <HomeCard
           navigation={navigation}
-          image={VOLUMETIMELINE}
+          image={VOLUMETIMELINE as any}
           title="Volume Timeline"
           description="XY timeline of Enron email per day with drill down."
           link="VolumeTimelineView"
         />
         <HomeCard
           navigation={navigation}
-          image={NETWORKGRAPH}
+          image={NETWORKGRAPH as any}
           title="Network Graph"
           description="Network graph of Enron key contact communication."
           link="NetworkGraphView"
         />
         <HomeCard
           navigation={navigation}
-          image={TREEMAP}
+          image={TREEMAP as any}
           title="Tree Map"
           description="Tree map of email volume of Enron key contacts."
           link="TreeMapView"
         />
         <HomeCard
           navigation={navigation}
-          image={EVENTTIMELINE}
+          image={EVENTTIMELINE as any}
           title="Event Timeline"
           description="Event timeline of Enron fraud and litigation."
           link="EventTimelineView"
         />
         <HomeCard
           navigation={navigation}
-          image={POLAR}
+          image={POLAR as any}
           title="Polar"
           description="Polar chart of email volume of Enron key contacts."
           link="PolarView"
         />
         <HomeCard
           navigation={navigation}
-          image={SEARCH}
+          image={SEARCH as any}
           title="Search"
           description="Full text search with field filtering and hit highlighting."
           link="SearchView"
         />
         <HomeCard
           navigation={navigation}
-          image={SEARCH}
+          image={SETTINGS as any}
           title="Settings"
           description="Application settings."
           link="AppSettingsView"
