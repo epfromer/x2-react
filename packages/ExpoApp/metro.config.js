e178971f7c4869c48fa8ab6f8517c4182f67cafa
Ed Pfromer <epfromer@gmail.com>
2020-08-08 08:16:50 -0600

    update

--- packages/ExpoApp/metro.config.js
+++ packages/ExpoApp/metro.config.js
@@ -1,3 +1,5 @@
 const { createMetroConfiguration } = require('expo-yarn-workspaces')
 
 module.exports = createMetroConfiguration(__dirname)
+
+// https://github.com/highcharts/highcharts-react-native/issues/93
--- packages/web-app/src/App.js
+++ packages/web-app/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './App.css'
-import { HelloWorld } from '@x2react/shared'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>{HelloWorld}, web-app</p>
+        <p>web-app</p>
       </header>
     </div>
   )
