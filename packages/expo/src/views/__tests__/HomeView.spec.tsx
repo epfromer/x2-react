a4977e6f816d3410a0e6bcf511c4a726849fe1c9
Ed Pfromer <epfromer@gmail.com>
2020-08-20 09:56:22 -0600

    update

--- packages/expo/src/views/__tests__/HomeView.spec.tsx
+++ packages/expo/src/views/__tests__/HomeView.spec.tsx
@@ -1,11 +1,13 @@
-import { fireEvent } from '@testing-library/react-native';
-import React from 'react';
-import { renderComp } from '../../setupTests';
-import HomeView from '../HomeView';
+import { fireEvent } from '@testing-library/react-native'
+import React from 'react'
+import { renderComp } from '../../setupTests'
+import HomeView from '../HomeView'
 
 test('HomeView', () => {
-  const { getByText } = renderComp(<HomeView navigation={{}} />)
+  const navigation = { navigate: jest.fn() }
+  const { getByText } = renderComp(<HomeView navigation={navigation} />)
   const card = getByText(/Pie chart/i)
   expect(card).not.toBeNull()
-  // fireEvent.press(card)
+  fireEvent.press(card)
+  expect(navigation.navigate).toHaveBeenCalledWith('PieView')
 })
