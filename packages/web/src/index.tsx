781c18b708ec838af3b429ec12b36c2035e0c6b4
Ed Pfromer <epfromer@gmail.com>
2020-08-25 08:28:56 -0600

    back out amcharts

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,5 +1,3 @@
-import * as am4core from '@amcharts/amcharts4/core'
-import am4themes_animated from '@amcharts/amcharts4/themes/animated'
 import MomentUtils from '@date-io/moment'
 import { store } from '@klonzo/common'
 import { MuiPickersUtilsProvider } from '@material-ui/pickers'
@@ -8,8 +6,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './App'
 import './index.css'
-
-am4core.useTheme(am4themes_animated)
 
 const AppWithPickers = () => (
   <MuiPickersUtilsProvider utils={MomentUtils}>
