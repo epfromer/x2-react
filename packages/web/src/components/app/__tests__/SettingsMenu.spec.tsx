de193f6758513e9ac0606111ddb00383756cf331
Ed Pfromer <epfromer@gmail.com>
2020-10-19 07:43:32 -0600

    update

--- packages/web/src/components/app/__tests__/SettingsMenu.spec.tsx
+++ packages/web/src/components/app/__tests__/SettingsMenu.spec.tsx
@@ -1,7 +1,22 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import { fireEvent } from '@testing-library/react'
 import React from 'react'
 import { renderComp } from '../../../setupTests'
 import SettingsMenu from '../SettingsMenu'
+
+jest.mock('@auth0/auth0-react')
+const user = {
+  email: 'johndoe@me.com',
+  email_verified: true,
+  sub: 'google-oauth2|2147627834623744883746',
+}
+// @ts-ignore
+useAuth0.mockReturnValue({
+  isAuthenticated: true,
+  user,
+  logout: jest.fn(),
+  loginWithRedirect: jest.fn(),
+})
 
 test('renders', async () => {
   const { getByTestId } = renderComp(<SettingsMenu />)
