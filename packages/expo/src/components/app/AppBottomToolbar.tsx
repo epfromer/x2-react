38301705da2fe12748f9403479bb803f89777555
Ed Pfromer <epfromer@gmail.com>
2020-10-19 18:10:54 -0600

    update

--- packages/expo/src/components/app/AppBottomToolbar.tsx
+++ packages/expo/src/components/app/AppBottomToolbar.tsx
@@ -32,7 +32,7 @@ export default function AppBottomToolbar() {
       .join('&')
 
   const handleLoginResponse = (response: any) => {
-    const decoded = jwtDecode(response.params.id_token)
+    const decoded: any = jwtDecode(response.params.id_token)
     if (response.type === 'success') {
       dispatch(setAuthenticated(true))
       dispatch(setUsername(decoded.email))
