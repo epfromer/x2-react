e4ff61264510e317b8610adbb687068afb47955c
Ed Pfromer <epfromer@gmail.com>
2020-10-21 11:01:24 -0600

    import log scroll into view

--- packages/web/src/components/ImportLog.tsx
+++ packages/web/src/components/ImportLog.tsx
@@ -7,30 +7,36 @@ import {
   x2Server,
 } from '@klonzo/common'
 import Button from '@material-ui/core/Button'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
 import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
 import { gql, request } from 'graphql-request'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 const useStyles = makeStyles((theme) => ({
-  root: { width: '100%', marginTop: theme.spacing(2) },
-  paper: { width: '100%', marginBottom: theme.spacing(2), maxHeight: 400 },
-  table: { minWidth: 350 },
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(2),
+    // position: 'relative',
+    overflow: 'auto',
+    maxHeight: 300,
+  },
   button: { margin: 15 },
 }))
 
 export default function ImportLog() {
   const log = useSelector(selectImportLog)
   const classes = useStyles()
+  const [lastRow, setLastRow] = useState<HTMLDivElement | undefined | null>()
 
   useEffect(() => stopImportStatusInterval, [])
+
+  useEffect(() => {
+    if (lastRow) lastRow.scrollIntoView({ behavior: 'smooth' })
+  })
 
   getImportStatus()
 
@@ -47,37 +53,30 @@ export default function ImportLog() {
   }
 
   return (
-    <div className={classes.root}>
-      <Paper>
-        <Button
-          variant="contained"
-          color="secondary"
-          className={classes.button}
-          onClick={startImport}
-        >
-          Import Email
-        </Button>
-        <TableContainer component={Paper} className={classes.paper}>
-          <Table size="small" aria-label="import-log">
-            <TableHead>
-              <TableRow>
-                <TableCell>Date</TableCell>
-                <TableCell>Import Log</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {log?.map((logEntry: ImportLogEntry) => {
-                return (
-                  <TableRow hover key={logEntry.id}>
-                    <TableCell>{logEntry.timestamp}</TableCell>
-                    <TableCell>{logEntry.entry}</TableCell>
-                  </TableRow>
-                )
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-    </div>
+    <Paper>
+      <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        onClick={startImport}
+      >
+        Import Email
+      </Button>
+      <List dense className={classes.root} aria-label="import-log">
+        {log?.map((logEntry: ImportLogEntry) => {
+          return (
+            <ListItem key={logEntry.id} alignItems="flex-start">
+              <ListItemText
+                primary={logEntry.timestamp + ' ' + logEntry.entry}
+              />
+            </ListItem>
+          )
+        })}
+        <div
+          style={{ float: 'left', clear: 'both' }}
+          ref={(el) => setLastRow(el)}
+        ></div>
+      </List>
+    </Paper>
   )
 }
