127e6dc52adffb6a79686342b138d820922ac6be
Ed Pfromer <epfromer@gmail.com>
2020-10-27 16:39:08 -0600

    update

--- packages/web/src/components/Highcharts/PieHighcharts.tsx
+++ packages/web/src/components/Highcharts/PieHighcharts.tsx
@@ -1,4 +1,4 @@
-import { EmailXferedDatum } from '@klonzo/common'
+import { EmailXferedDatum, getPieHighchartsConfig } from '@klonzo/common'
 import { useTheme } from '@material-ui/core/styles'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
@@ -20,13 +20,13 @@ export default function PieHighcharts({
 }: Props) {
   const theme = useTheme()
 
-  interface HighChartsDatum {
+  interface HighchartsDatum {
     name: string
     y: number
     color: string
     events: any
   }
-  const custodians: Array<HighChartsDatum> = []
+  const custodians: Array<HighchartsDatum> = []
   data.forEach((datum) => {
     custodians.push({
       name: datum.name,
@@ -38,46 +38,17 @@ export default function PieHighcharts({
     })
   })
 
-  const config = {
-    chart: {
-      plotBackgroundColor: null,
-      plotBorderWidth: null,
-      plotShadow: false,
-      type: 'pie',
-      backgroundColor: theme.palette.background.default,
-    },
-    title: {
-      text: title,
-      style: {
-        color: theme.palette.text.primary,
-      },
-    },
-    tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
-    },
-    accessibility: {
-      point: {
-        valueSuffix: '%',
-      },
-    },
-    plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-          enabled: true,
-          format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-        },
-      },
-    },
-    series: [
-      {
-        name: 'Custodians',
-        colorByPoint: true,
-        data: custodians,
-      },
-    ],
-  }
-
-  return <HighchartsReact highcharts={Highcharts} options={config} />
+  return (
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={getPieHighchartsConfig(
+        theme.palette.text.primary,
+        title,
+        search,
+        data,
+        theme.palette.background.default,
+        handleClick
+      )}
+    />
+  )
 }
