11ff8d0c2cf908c129d002be69ee489314d9dd64
Ed Pfromer <epfromer@gmail.com>
2021-03-13 08:21:15 -0700

    bringing in sync with flutter app

--- packages/expo/src/components/app/AppTopToolbar.tsx
+++ packages/expo/src/components/app/AppTopToolbar.tsx
@@ -1,13 +1,16 @@
+import { getDarkMode, setDarkModeAsync, store } from '@klonzo/common'
 import React, { useContext } from 'react'
 import { StyleSheet } from 'react-native'
 import { Button, Header, Icon, ThemeContext } from 'react-native-elements'
+import { useSelector } from 'react-redux'
 import { useHistory, useLocation } from 'react-router-native'
-import { textColor } from '../../utils/appThemes'
 import { routeNames } from '../../router/AppRouting'
+import { textColor } from '../../utils/appThemes'
 
 export default function AppTopToolbar() {
   const location = useLocation()
   const history = useHistory()
+  const darkMode = useSelector(getDarkMode)
   const { theme }: any = useContext(ThemeContext)
 
   const styles = StyleSheet.create({
@@ -28,12 +31,18 @@ export default function AppTopToolbar() {
     />
   )
 
-  const HomeIconButton = () => (
+  const DarkModeButton = () => (
     <Button
-      onPress={() => history.push('/')}
+      onPress={() => setDarkModeAsync(store, darkMode ? false : true)}
       buttonStyle={styles.button}
-      icon={<Icon name="home" size={25} color={textColor(theme)} />}
-      testID="home-button"
+      icon={
+        <Icon
+          name={darkMode ? 'brightness-high' : 'brightness-4'}
+          size={25}
+          color={textColor(theme)}
+        />
+      }
+      testID="dark-mode-button"
     />
   )
 
@@ -47,7 +56,7 @@ export default function AppTopToolbar() {
         text: routeNames[location.pathname],
         style: styles.header,
       }}
-      rightComponent={<HomeIconButton />}
+      rightComponent={<DarkModeButton />}
     />
   )
 }
