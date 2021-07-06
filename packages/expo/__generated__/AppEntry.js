007e693859d7144cf168692d9b2b10458cb346b2
Ed Pfromer <epfromer@gmail.com>
2021-07-05 19:57:42 -0600

    update

--- packages/expo/__generated__/AppEntry.js
+++ packages/expo/__generated__/AppEntry.js
@@ -4,7 +4,7 @@ import 'expo/build/Expo.fx';
 import { activateKeepAwake } from 'expo-keep-awake';
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 
-import App from '../App';
+import App from '..\..\..\node_modules\@klonzo\expo/App';
 
 if (__DEV__) {
   activateKeepAwake();
