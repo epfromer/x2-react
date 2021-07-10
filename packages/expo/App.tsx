db99de86016de92fbf53e26a28e3dfa9776d3d42
Ed Pfromer <epfromer@gmail.com>
2021-07-10 17:15:21 -0600

    update

--- packages/expo/App.tsx
+++ packages/expo/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>x2-react!! aa</Text>
+      <Text>x2-react!!</Text>
     </View>
   )
 }
