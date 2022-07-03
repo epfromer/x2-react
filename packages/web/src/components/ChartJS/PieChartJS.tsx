88971352a11905b31639d9703c8ff213857bcda1
Ed Pfromer <epfromer@gmail.com>
2022-07-03 08:43:13 -0600

    update

--- packages/web/src/components/ChartJS/PieChartJS.tsx
+++ packages/web/src/components/ChartJS/PieChartJS.tsx
@@ -1,10 +1,6 @@
-import { EmailXferedDatum, getDarkMode } from '@klonzo/common'
+import { EmailXferedDatum } from '@klonzo/common'
 import { useTheme } from '@mui/material/styles'
-import { Chart } from 'chart.js'
-import { useEffect, useRef } from 'react'
-import { useSelector } from 'react-redux'
-
-const chartHeight = '250'
+import { Pie } from 'react-chartjs-2'
 
 interface Props {
   title: string
@@ -18,27 +14,22 @@ export default function PieChartJS({
   data,
   handleClick,
 }: Props) {
-  const chartContainer: any = useRef(null)
-  const darkMode = useSelector(getDarkMode)
   const theme = useTheme()
 
-  const config: any = {
-    type: 'pie',
-    options: {
-      plugins: {
-        title: {
-          display: true,
-          padding: { bottom: 10 },
-          color: theme.palette.text.primary,
-          font: { size: 18 },
-          text: title,
-        },
+  const options: any = {
+    plugins: {
+      title: {
+        display: true,
+        padding: { bottom: 10 },
+        color: theme.palette.text.primary,
+        font: { size: 18 },
+        text: title,
       },
-      legend: {
-        position: 'bottom',
-        labels: {
-          color: theme.palette.text.primary,
-        },
+    },
+    legend: {
+      position: 'bottom',
+      labels: {
+        color: theme.palette.text.primary,
       },
     },
     onClick: (e: any, item: any) => {
@@ -46,31 +37,17 @@ export default function PieChartJS({
         handleClick(search, data[item[0].index].name)
       }
     },
-    data: {
-      labels: data.map((datum) => datum.name),
-      datasets: [
-        {
-          backgroundColor: data.map((datum) => datum.color),
-          data: data.map((datum) => datum.value),
-        },
-      ],
-    },
   }
 
-  useEffect(() => {
-    let instance: Chart
-    if (chartContainer && chartContainer.current) {
-      instance = new Chart(chartContainer.current, config)
-    }
-    return () => {
-      if (instance) instance.destroy()
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chartContainer, darkMode])
+  const chartData: any = {
+    labels: data.map((datum) => datum.name),
+    datasets: [
+      {
+        backgroundColor: data.map((datum) => datum.color),
+        data: data.map((datum) => datum.value),
+      },
+    ],
+  }
 
-  return (
-    <div>
-      <canvas height={chartHeight} ref={chartContainer} />
-    </div>
-  )
+  return <Pie options={options} data={chartData} />
 }
