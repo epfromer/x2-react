9bfe40cde99f10fc1667dd728af492e7b186c567
Ed Pfromer <epfromer@gmail.com>
2022-09-21 14:53:15 -0600

    update

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -1,7 +1,8 @@
 export const defaultLimit = 50
 export const defaultThemeName = 'Blue'
 export const blackBackground = '#222222'
-export const importLoc = 'c:/github/outlook/'
+export const importLoc = 'm:/outlook/edrm/'
 export const maxFromLength = 25
 export const maxSubjectLength = 40
-export const x2Server = 'https://x2-server-mongodb.herokuapp.com'
+export const x2Server = 'http://localhost:3000'
+// export const x2Server = 'https://x2-server-mongodb.herokuapp.com'
